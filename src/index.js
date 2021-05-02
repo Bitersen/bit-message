80e5f88d4b9e7ad5ffed5925f276e3145c585a4a
Vladimir Jovanović <vladimir@webdizajn.org>
2021-05-02 20:34:22 +0200

    Refactor the plugin
    - Add 'assert' method

--- src/index.js
+++ src/index.js
@@ -1,72 +1,90 @@
 const chalk = require('chalk')
 const boxen = require('boxen')
+const isFalsy = require('./isFalsy')
+
+const BOXEN_CONFIG = {
+  style: {
+    padding: 1,
+    margin: {
+      top: 1,
+      right: 0,
+      bottom: 1,
+      left: 0
+    },
+    borderStyle: 'round',
+  },
+  log: {
+    borderColor: 'green',
+  },
+  info: {
+    borderColor: 'blue',
+  },
+  warn: {
+    borderColor: 'yellow',
+  },
+  error: {
+    borderColor: 'red',
+  },
+}
+
+const log = (...messages) => {
+  console.log(boxen(
+    messages.join(`\n`),
+    {
+      ...BOXEN_CONFIG.style,
+      ...BOXEN_CONFIG.log
+    }
+  ))
+}
+
+const info = (...messages) => {
+  console.info(boxen(
+    chalk.blue(messages.join(`\n`)),
+    {
+      ...BOXEN_CONFIG.style,
+      ...BOXEN_CONFIG.info
+    }
+  ))
+}
+
+const warn = (...messages) => {
+  console.warn(boxen(
+    chalk.yellow(messages.join(`\n`)),
+    {
+      ...BOXEN_CONFIG.style,
+      ...BOXEN_CONFIG.warn
+    }
+  ))
+}
+
+const error = (...messages) => {
+  console.error(boxen(
+    chalk.red(messages.join(`\n`)),
+    {
+      ...BOXEN_CONFIG.style,
+      ...BOXEN_CONFIG.error
+    }
+  ))
+}
+
+const assert = (condition = true, ...messages) => {
+  if (isFalsy(condition)) {
+    console.log(
+      boxen(
+        chalk.red(messages.join(`\n`)),
+        {
+          ...BOXEN_CONFIG.style,
+          ...BOXEN_CONFIG.error
+        }
+      )
+    )
+  }
+}
 
 module.exports = {
-	// Boxen configuration
-	boxenConfig: {
-		style: {
-			padding: 1,
-			margin: {
-				top: 1,
-				right: 0,
-				bottom: 1,
-				left: 0
-			},
-			borderStyle: 'round'
-		},
-    log: {
-      borderColor: 'green'
-    },
-    info: {
-      borderColor: 'blue'
-    },
-    warn: {
-      borderColor: 'yellow'
-    },
-		error: {
-			borderColor: 'red'
-		}
-	},
-
-	// Methods
-  log (message) {
-    console.log(boxen(
-      message,
-      {
-        ...this.boxenConfig.style,
-        ...this.boxenConfig.log
-      }
-    ))
-  },
-
-	info (message) {
-		console.info(boxen(
-		chalk.blue(message),
-			{
-				...this.boxenConfig.style,
-				...this.boxenConfig.info
-			}
-		))
-	},
-
-
-	warn (message) {
-		console.error(boxen(
-			chalk.yellow(message),
-			{
-				...this.boxenConfig.style,
-				...this.boxenConfig.warn
-			}
-		))
-	},
-
-	error (message) {
-		console.error(boxen(
-			chalk.red(message),
-			{
-				...this.boxenConfig.style,
-				...this.boxenConfig.error
-			}
-		))
-	}
+  log,
+  info,
+  warn,
+  error,
+  assert,
 }
